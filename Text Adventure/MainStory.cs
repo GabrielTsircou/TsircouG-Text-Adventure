e03a79dfabcf7ce1020a53748885e60e248d0ca4
gtsircou <1722710@mukilteo.wednet.edu>
2024-10-08 10:20:47 -0700

    completely remodeled code structure, now calls all story from StoryParts.cs, also started work on main story

---
+++ Text Adventure/MainStory.cs
@@ -0,0 +1,126 @@
+﻿using StoryParts;
+
+namespace Text_Adventure
+{
+    public class Program
+    {
+        public bool bootUp = false;
+        public static void staggerText(string text, int staggerTime = 100)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.Write(text[i]);
+                Thread.Sleep(staggerTime);
+            }
+            Console.WriteLine();
+        }
+        public static void Main(string[] args)
+        {
+            Console.SetWindowSize(150, 30);
+            parts.MainMenu();
+            //makes input from the console readable
+            string input = Console.ReadLine();
+            //spits out the input
+            if (input == "DEBUG")
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("NOW BOOTING: [DEBUG]");
+                Console.ResetColor();
+                staggerText("...", 500);
+                Console.WriteLine("BOOTING UP TEST SIMULATION...");
+                staggerText("...", 500);
+                Console.WriteLine("please work");
+                Console.WriteLine("[RECOLOR] or [CALL] or [WAIT] or [BRANCH]");
+                input = Console.ReadLine();
+
+                if (input == "RECOLOR")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("yooooo shits red now");
+                    Console.ResetColor();
+                    Console.WriteLine("back to normal now");
+                }
+                else if (input == "CALL")
+                {
+                    parts.Please();
+                }
+                else if (input == "ALL CAPS!")
+                {
+                    Console.WriteLine("its supposed to load the music video for MF DOOM's ''All Caps'' lol");
+                }
+                else if (input == "WAIT")
+                {
+                    Thread.Sleep(5000);
+                    Console.WriteLine("You wasted 5 seconds");
+                    staggerText("...", 1000);
+
+                }
+                else if (input == "BRANCH")
+                {
+                    Console.WriteLine("[3] or [4] or [even more]");
+                    input = Console.ReadLine();
+                    if (input == "3")
+                    {
+                        Console.WriteLine("boooo");
+                    }
+                    else if (input == "4")
+                    {
+                        Console.WriteLine("why cant i write functional code");
+                    }
+                    else if (input == "even more")
+                    {
+                        Console.WriteLine("[5] or [6]");
+                        input = Console.ReadLine();
+                        if (input == "5")
+                        {
+                            Console.WriteLine("please let the nightmare end");
+                        }
+                        else if (input == "6")
+                        {
+                            Console.WriteLine("please let me die");
+                        }
+                        else
+                        {
+                            Console.WriteLine("GRAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("WRONG ANSWER *POW!* *POW!* *POW!* *POW!* *POW!* *POW!* *POW!* *POW!* *reload* *POW!* *POW!* *POW!* *POW!* *POW!* *P");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("BOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+                }
+            }
+            else
+            {
+                parts.Intro();
+                input = Console.ReadLine();
+                if (input == "check")
+                {
+                    parts.InCheck();
+                    input = Console.ReadLine();
+                    if (input == "")
+                    {
+                        parts.BootUp();
+                    }
+                    else
+                    {
+                        parts.BootUp();
+                    }
+                }
+                else if (input == "ask")
+                {
+                    parts.InAsk();
+                }
+                else if (input == "guess")
+                {
+                    parts.InGuess();
+                }
+            }
+        }
+    }
+}
---
+++ Text Adventure/StoryParts.cs
@@ -0,0 +1,128 @@
+﻿using Text_Adventure;
+
+namespace StoryParts
+{
+    public class parts
+    {
+        public static void Please()
+        {
+            Program.staggerText("if you can read this somethings working!");
+        }
+        public static void MainMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("_____ __ __ ______ _____  __     _____ _____ _____");
+            Thread.Sleep(200);
+            Console.WriteLine("||  | ||  | ||     ||   | ||     ||  | ||  | ||  |");
+            Thread.Sleep(200);
+            Console.WriteLine("||  | ||  | ||__   ||--/  ||     ||  | ||--| ||  |");
+            Thread.Sleep(200);
+            Console.WriteLine("||  | || /  ||     ||   | ||     ||  | ||  | ||  |");
+            Thread.Sleep(200);
+            Console.WriteLine("||__| ||/   ||____ ||   | ||____ ||__| ||  | ||__/");
+            Thread.Sleep(100);
+            Console.ResetColor();
+            Thread.Sleep(100);
+            Console.WriteLine("");
+            Console.WriteLine("By GABRIEL TSIRCOU");
+            Console.WriteLine("");
+            Thread.Sleep(300);
+            Console.WriteLine("Narration are in (Parentheses), speech are in plaintext and are in different colors for different people...");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("...And options to pick from are in yellow and in [brackets].");
+            Console.ResetColor();
+            Console.WriteLine("Make sure you don't misspell anything, as it'll fail and you'll probably have to restart.");
+            Console.WriteLine("(HINT: Make sure it's in all lowercase!)");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Press [ENTER] to begin.");
+        }
+        public static void Intro()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("NOW BOOTING: [MAIN STORY]");
+            Console.ResetColor();
+            Thread.Sleep(1000);
+            Console.WriteLine("BEGINNING SIMULATION...");
+            Program.staggerText("...", 500);
+            Console.Clear();
+            Console.WriteLine("(It's 1999, and it's your first day at the newly built Howard's Point Nuclear Fission Plant as Chief Engineer.)");
+            Thread.Sleep(1000);
+            Console.WriteLine("(You walk up to the center console and turn it on.)");
+            Thread.Sleep(1000);
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("");
+            Console.WriteLine("Please Enter Credentials:");
+            Console.ResetColor();
+            Thread.Sleep(1000);
+            Console.WriteLine("");
+            Console.WriteLine("(You don't remember your passkey. You try not to stress about it as it was given to you an hour ago, but you still feel bad)");
+            Thread.Sleep(1000);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("");
+            Console.WriteLine("([CHECK] your pockets for notes, [ASK] someone for help, or [GUESS] and hope you get lucky.)");
+            Console.ResetColor();
+        }
+        public static void InCheck()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("(You check the insides of your pockets, and find a note from your supervisor. It reads-)");
+            Console.WriteLine("");
+            Thread.Sleep(1000);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Congrats on getting the job, rook! If you need help signing in, just type in your birthday, you'll need to change it by the end of the week.");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Thread.Sleep(1000);
+            Console.WriteLine("(You're not sure if they're asking you to change your passkey or your birthday, but you type it in and it miraculously works.)");
+            Thread.Sleep(1000);
+            Console.WriteLine("(You sit as the console boots up.)");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine();
+            Console.WriteLine("(Press [ENTER] to continue.)");
+        }
+        public static void InAsk()
+        {
+            Console.WriteLine("(You call someone over to help you sign in to the console.)");
+        }
+        public static void InGuess()
+        {
+            Console.WriteLine("(You give up trying to remember your password and guess something random.");
+
+        }
+        public static void BootUp()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Beep();
+            Console.WriteLine("     ________   ___    __   _______   _____   ___    __   ________");
+            Thread.Sleep(100);
+            Console.WriteLine("    //      /   //|    /   //          //     //|    /   //");
+            Thread.Sleep(100);
+            Console.WriteLine("   //======/   // |   /   //_____     //     // |   /   //__");
+            Thread.Sleep(100);
+            Console.WriteLine("  //      /   //  |  /          /    //     //  |  /   //   ");
+            Thread.Sleep(100);
+            Console.WriteLine(" //      /   //   | /          /    //     //   | /   //");
+            Thread.Sleep(100);
+            Console.WriteLine("//      /  _//_  _|/_   ______/  __//__  _//_  _|/_  //______");
+            Console.WriteLine("ANSINE CORP.TM COPYRIGHT 1997");
+            Thread.Sleep(100);
+            Program.staggerText("...", 500);
+            Console.WriteLine("");
+            Console.WriteLine("WELCOME TO ANSINE CORP.TM REACTOR CONTROL CONSOLE, VER. 1.8.4");
+            Thread.Sleep(300);
+            Console.WriteLine("");
+            Console.WriteLine("Please Select an Command:");
+            Console.WriteLine("");
+            Thread.Sleep(100);
+            Console.WriteLine("[1] STARTUP SEQUENCE");
+            Console.WriteLine("[2] REACTOR STATUS");
+            Console.WriteLine("[3] LOGS");
+            Console.WriteLine("[4] EMERGENCY PROGRAMS");
+            Console.WriteLine("[5] MANUAL");
+            Console.WriteLine("[6] QUIT");
+        }
+    }
+}
